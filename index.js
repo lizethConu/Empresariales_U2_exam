5bd94dfeefe16d3a713b08bc76a16adf635b4cf5
lizeth <lizeth.conu@gmail.com>
2019-03-29 09:03:05 -0600

    examen u2

---
+++ index.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/exam');
+
+
+var kittySchema = new mongoose.Schema({
+    code:{ type:String, required:true},
+    price: {type: Number, required:true},
+    description: { type:String, required:true},
+    quantity: { type:Number, required:true},
+    minimun: { type:Number, required:true},
+  });
+
+  var Kitten = mongoose.model('Kitten', kittySchema, 'kitten');
+  var puff = new Kitten({code: '123',pice:53,description:'this is a exam',quantity:5,minimun:10});
+
+
+  puff.save(function ( fluffy,err) {
+    if (err) return console.log(err);
+    else console.log("inserccion exitosa");
+  });
+
+  Kitten.find({}, function (err, kittens) {
+    if (err) return console.log(err);
+    else{
+        if(kittens.quantity<kittens.minimun){
+          console.log(kittens);
+        }      
+    } 
+  });
